1d491c1f4023da0ec502ae1790a099f7ade5fa1a
Justin Venegas <RL190010@stu.rlas-116.org>
2019-10-08 12:38:02 -0500

    initial commit

---
+++ multiplyColaborate/ViewControllerFindArea.swift
@@ -0,0 +1,29 @@
+//
+//  ViewControllerFindArea.swift
+//  multiplyColaborate
+//
+//  Created by JUSTIN VENEGAS on 10/8/19.
+//  Copyright © 2019 JUSTIN VENEGAS. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerFindArea: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var widthTextField: UITextField!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+    
+
+}
